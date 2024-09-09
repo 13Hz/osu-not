dc8cec635cdc79bde9cfb32bb4601f9790c8a3cf
13Hz <async13hhz@gmail.com>
2024-09-09 15:57:05 +0300

    Поправил форматирование сообщения (сложность)

--- app/Schedules/CheckUsersScoresSchedule.php
+++ app/Schedules/CheckUsersScoresSchedule.php
@@ -29,7 +29,7 @@ class CheckUsersScoresSchedule
                         //TODO: Добавить обложку карты + инфу по показателям AR CS OD и тд + ссылку на профиль, карту, скор
                         Telegram::sendMessage([
                             'chat_id' => $chat->id,
-                            'text' => "$lastScore->rank | [$user->name](https://osu.ppy.sh/users/$user->id) | {$lastScore->beatmapset->artist} - {$lastScore->beatmapset->title} [{$lastScore->beatmap->version}] | {$pp}pp $accuracy% {$lastScore->beatmap->difficulty_rating}✩ $mods",
+                            'text' => "$lastScore->rank | [$user->name](https://osu.ppy.sh/users/$user->id) | {$lastScore->beatmapset->artist} - {$lastScore->beatmapset->title} \[{$lastScore->beatmap->version}] | {$pp}pp $accuracy% {$lastScore->beatmap->difficulty_rating}✩ $mods",
                             'parse_mode' => 'markdown',
                             'disable_web_page_preview' => true
                         ]);

75e2f8c748373001bf93c5d12933173fbd947b8b
13Hz <async13hhz@gmail.com>
2024-09-20 16:46:59 +0300

    Автоочистка устаревших батчей

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Schedule;
 use Spatie\ShortSchedule\Facades\ShortSchedule;
 
 ShortSchedule::command('app:check-players-last-score')->everySeconds(config('api.players_check_delay'))->withoutOverlapping();
+Schedule::command('app:delete-finished-batches')->everyMinute()->withoutOverlapping();

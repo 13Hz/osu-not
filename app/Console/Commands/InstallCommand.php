e49dcb2fcf4404ce60031a9a920999b7d36cd49a
13Hz <13Hz@users.noreply.github.com>
2024-11-01 14:53:37 +0000

    CS Fix

--- app/Console/Commands/InstallCommand.php
+++ app/Console/Commands/InstallCommand.php
@@ -127,7 +127,7 @@ class InstallCommand extends Command
 
     private function getOauthTokenLink(): void
     {
-        $this->components->task('Обновляю конфигурацию', function() {
+        $this->components->task('Обновляю конфигурацию', function () {
             $this->artisan->call('config:clear');
             $this->artisan->call('config:cache');
         });

eb6e0a88a0a438955e847ea4eb5a24cfae6892af
13Hz <async13hhz@gmail.com>
2024-11-05 15:57:44 +0300

    Убрал проверку кэша при запуске обновления данных

--- app/Console/Commands/CheckPlayersLastScoreCommand.php
+++ app/Console/Commands/CheckPlayersLastScoreCommand.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Bus\Batch;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\LazyCollection;
 use Throwable;
@@ -18,17 +18,32 @@ class CheckPlayersLastScoreCommand extends Command
 
     protected $description = 'Команда для создания очереди для проверки пользователей на наличие новых результатов';
 
+    private const BATCH_NAME = 'check-players-last-score';
     private const CHUNK_SIZE = 5;
+    private ?string $lastBatchId;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->lastBatchId = DB::table('job_batches')
+            ->where('name', '=', self::BATCH_NAME)
+            ->latest()
+            ->value('id');
+    }
 
     public function handle()
     {
-        if (Cache::has('batch_running')) {
-            return self::SUCCESS;
+        if ($this->lastBatchId) {
+            $lastBatch = Bus::findBatch($this->lastBatchId);
+            if ($lastBatch && !$lastBatch->finished()) {
+                $this->info('Обновление уже запущено, жду завершения');
+
+                return self::SUCCESS;
+            }
         }
 
-        Cache::put('batch_running', true, 3600);
-
-        $batch = Bus::batch([]);
+        $batch = Bus::batch([])->name(self::BATCH_NAME);
 
         User::all()->lazy()->pluck('id')->chunk(self::CHUNK_SIZE)->each(function (LazyCollection $collection) use ($batch) {
             $batch->add(new CheckPlayerLastScoreJob($collection->toArray()));
@@ -36,8 +51,6 @@ class CheckPlayersLastScoreCommand extends Command
 
         $batch->catch(function (Batch $batch, Throwable $throwable) {
             Log::warning('Ошибка выполнения задачи ' . $throwable->getMessage());
-        })->finally(function () {
-            Cache::forget('batch_running');
         })->dispatch();
 
         return self::SUCCESS;

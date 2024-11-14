22077e400fa038e9707da07642d9953d8ea43604
13Hz <async13hhz@gmail.com>
2024-11-14 15:41:42 +0300

    фикс

--- database/migrations/2024_11_14_152526_add_last_score_check_at_field_in_users_table.php
+++ database/migrations/2024_11_14_152526_add_last_score_check_at_field_in_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dateTime('last_score_check_at')->nullable();
+            $table->dateTime('last_score_check_at')->default(now());
         });
     }
 
